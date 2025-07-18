dfdd2b115b57b00da3594b89a70e69cb68f89e6d
deskolado <discorddeskolado@gmail.com>
2025-07-18 09:47:31 -0300

    Update storage.py

--- utils/storage.py
+++ utils/storage.py
@@ -4,8 +4,6 @@ import os
 def salvar_dados(df):
     path = "model/dataset.csv"
     if os.path.exists(path):
-        df_antigo = pd.read_csv(path)
-        df_novo = pd.concat([df_antigo, df], ignore_index=True)
-    else:
-        df_novo = df
-    df_novo.to_csv(path, index=False)
+        antigo = pd.read_csv(path)
+        df = pd.concat([antigo, df], ignore_index=True)
+    df.to_csv(path, index=False)
