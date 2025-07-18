8f37f998d2cac9636547ef5eb0cd755e7e2c13ae
deskolado <discorddeskolado@gmail.com>
2025-07-18 00:52:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, request
-from PIL import Image
-import io
 import os
 from werkzeug.utils import secure_filename
-import datetime
 from utils.image_analysis import analisar_imagem_e_gerar_sinal
 
 app = Flask(__name__)
@@ -16,22 +13,12 @@ if not os.path.exists(app.config['UPLOAD_FOLDER']):
 def index():
     resultado = None
     if request.method == 'POST':
-        if 'imagem' not in request.files:
-            return render_template('index.html', erro='Nenhuma imagem enviada.')
-
-        file = request.files['imagem']
-        if file.filename == '':
-            return render_template('index.html', erro='Nome de arquivo inválido.')
-
-        filename = secure_filename(file.filename)
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(filepath)
-
-        try:
+        file = request.files.get('imagem')
+        if file and file.filename:
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
             resultado = analisar_imagem_e_gerar_sinal(filepath)
-        except Exception as e:
-            resultado = {'erro': f'Erro ao processar a imagem: {str(e)}'}
-
     return render_template('index.html', resultado=resultado)
 
 if __name__ == '__main__':
