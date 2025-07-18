e6b5b941a7a95f59a2cb9ea8bc2c9f0aff5d87dd
deskolado <discorddeskolado@gmail.com>
2025-07-18 01:13:20 -0300

    Update trainer.py

--- model/trainer.py
+++ model/trainer.py
@@ -1,26 +1,15 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 import joblib
 import os
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.preprocessing import LabelEncoder
 
-def treinar_modelo():
-    if not os.path.exists("model/dataset.csv"):
-        return
+def extrair_recursos(df):
+    df = df.copy()
+    df['cor_cod'] = LabelEncoder().fit_transform(df['cor'])
 
-    df = pd.read_csv("model/dataset.csv")
-    if len(df) < 30:
-        return
-
-    df = df.dropna()
-    le_cor = LabelEncoder()
-    df['cor'] = le_cor.fit_transform(df['cor'])
-
-    X = df[['numero']]  # você pode incluir horário e padrões futuros aqui
-    y = df['cor']
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    modelo = RandomForestClassifier()
-    modelo.fit(X_train, y_train)
-    joblib.dump((modelo, le_cor), 'model/modelo.pkl')
+    # Hora em minutos
+    df['minuto'] = pd.to_datetime(df['horario'], format='%H:%M').dt.minute
+    df['hora'] = pd.to_datetime(df['horario'], format='%H:%M').dt.hour
+    df['momento'] = df['hora]()
